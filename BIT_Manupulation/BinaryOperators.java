560d4b97c2d3fccf1b323e03eca533fe240d4890
Rahul Singh Parihar <105512629+Rahul-Parihar@users.noreply.github.com>
2022-08-25 12:27:38 +0530

    Add files via upload

---
+++ BIT_Manupulation/BinaryOperators.java
@@ -0,0 +1,26 @@
+public class BinaryOperators {
+    public static void main(String[] args) {
+        // Binary AND
+        System.out.println(5 & 6);
+
+        // Binary OR
+
+        System.out.println(5 | 6);
+
+        // Binary XOR
+
+        System.out.println(5 ^ 6);
+
+        // Binary 1's complement
+
+        System.out.println((~5));
+
+        // Binary Left Shift
+
+        System.out.println(5 << 2);
+
+        // Binary Right Shift
+
+        System.out.println(6 >> 1);
+    }
+}
