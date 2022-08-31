2f11553683a227dc0ad492b42c3f7cdb22b4eb44
Rahul Singh Parihar <105512629+Rahul-Parihar@users.noreply.github.com>
2022-08-31 12:52:27 +0530

    Add files via upload

---
+++ Classes & Objects/Packages.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+public class Packages {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        System.out.println(a);
+        scn.close();
+
+    }
+
+}
