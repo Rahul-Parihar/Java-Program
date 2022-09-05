71049d87c0e52685e2af475f36cee75256984231
Rahul Singh Parihar <105512629+Rahul-Parihar@users.noreply.github.com>
2022-09-05 14:49:29 +0530

    Add files via upload

---
+++ Recursion/XPowN.java
@@ -0,0 +1,21 @@
+public class XPowN {
+    public static int Power(int x, int n) {
+        if (n == 0) {
+            return 1;
+
+        }
+
+        // int xnm1=Power(x, n-1);
+        // int xn=x*xnm1;
+        // return xn; // same in one line
+
+        return x * Power(x, n - 1);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Power(2, 10));
+
+    }
+
+}
