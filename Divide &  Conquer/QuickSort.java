d1838b7237a3b600c99fb1a75a5474db9f729afe
Rahul Singh Parihar <105512629+Rahul-Parihar@users.noreply.github.com>
2022-09-07 12:16:35 +0530

    Add files via upload

---
+++ Divide &  Conquer/QuickSort.java
@@ -0,0 +1,48 @@
+public class QuickSort {
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void quickSort(int arr[], int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
+
+        // last element
+        int PIdx = partition(arr, si, ei);
+        quickSort(arr, si, PIdx - 1); // left part
+        quickSort(arr, PIdx + 1, ei); // right part
+
+    }
+
+    public static int partition(int arr[], int si, int ei) {
+        int pivot = arr[ei];
+        int i = si - 1;// to make place for element smaller than pivot
+
+        for (int j = si; j < ei; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                // swap
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 6, 3, 9, 5, 2, 8 };
+        quickSort(arr, 0, arr.length - 1);
+        printArr(arr);
+
+    }
+}
